dd894f7f4c09431c61918b69a2c4db230bf929c2
Anton <anton.sukhovatkin@rmp.uhn.ca>
2022-11-18 16:07:23 -0500

    trials wip

--- apps/web/pages/trials/index.tsx
+++ apps/web/pages/trials/index.tsx
@@ -1,4 +1,5 @@
 import styles from './index.module.scss';
+import { Button } from 'primereact/button';
 
 import TopBar from "../../components/trials/TopBar";
 
@@ -7,6 +8,16 @@ const Trials = () => {
   return (
     <>
       <TopBar />
+      <div className={styles.pageContainer}>
+        <div className={styles.titleAndButtonsContainer}>
+          <span className={styles.trialsText}>Trials</span>
+          <div className={styles.buttonsContainer}>
+            <Button label="Import" className="p-button-text p-button-plain" />
+            <Button label="Create CTML" className={styles.createCtmlButton} />
+          </div>
+
+        </div>
+      </div>
     </>
 
 
