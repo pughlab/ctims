1881d5efc478463f50ea9de98bd125bde6c2fbcb
Anton <anton.sukhovatkin@rmp.uhn.ca>
2022-11-18 16:18:56 -0500

    trials table wip

--- apps/web/pages/trials/index.tsx
+++ apps/web/pages/trials/index.tsx
@@ -1,9 +1,36 @@
 import styles from './index.module.scss';
 import { Button } from 'primereact/button';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 import TopBar from "../../components/trials/TopBar";
+import {useEffect, useState} from "react";
 
 const Trials = () => {
+
+  const [trials, setTrials] = useState<any>([]);
+
+  useEffect(() => {
+    const trials = [
+      {
+        id: 1,
+        nickname: 'Trial 1',
+        principalInvestigator: 'Dr. John Doe',
+        status: 'Active',
+        createdOn: '2021-01-01 by Dr. John Doe',
+        updatedOn: '2021-01-01 by Dr. John Doe',
+      },
+      {
+        id: 2,
+        nickname: 'Trial 2',
+        principalInvestigator: 'Dr. John Doe',
+        status: 'Active',
+        createdOn: '2021-01-01 by Dr. John Doe',
+        updatedOn: '2021-01-01 by Dr. John Doe',
+      }
+    ]
+    setTrials(trials);
+  }, []);
 
   return (
     <>
@@ -16,6 +43,16 @@ const Trials = () => {
             <Button label="Create CTML" className={styles.createCtmlButton} />
           </div>
 
+        </div>
+        <div className={styles.tableContainer}>
+          <DataTable value={trials}>
+            <Column field="id" header="ID"></Column>
+            <Column field="nickname" header="Nickname"></Column>
+            <Column field="principalInvestigator" header="Principal Investigator"></Column>
+            <Column field="status" header="Status"></Column>
+            <Column field="createdOn" header="Created on"></Column>
+            <Column field="updatedOn" header="Modified on"></Column>
+          </DataTable>
         </div>
       </div>
     </>
