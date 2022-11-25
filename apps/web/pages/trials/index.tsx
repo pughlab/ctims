ca8b3a60ae5dbbfc7c0e1d505d2f7a5f9fa1ddef
Mickey Ng <mickey.ng@rmp.uhn.ca>
2022-11-25 10:39:03 -0500

    add hidden button next to nickname (WIP need to style ellipse button)

--- apps/web/pages/trials/index.tsx
+++ apps/web/pages/trials/index.tsx
@@ -4,8 +4,10 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 import TopBar from "../../components/trials/TopBar";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useRouter} from "next/router";
+import React from 'react';
+import {Menu} from "primereact/menu";
 
 const Trials = () => {
 
@@ -13,10 +15,36 @@ const Trials = () => {
 
   const router = useRouter();
 
+  const menu = useRef(null);
+
   const createCtmlClick = (e) => {
     e.preventDefault();
     router.push('/trials/create');
   }
+
+  const trialMenuItems = [
+    {
+      label: 'Edit',
+      icon: 'pi pi-pencil',
+      command: () => {
+        console.log('Edit');
+      }
+    },
+    {
+      label: 'Delete',
+      icon: 'pi pi-trash',
+      command: () => {
+        console.log('Edit');
+      }
+    },
+    {
+      label: 'Export',
+      icon: 'pi pi-upload',
+      command: () => {
+        console.log('Edit');
+      }
+    }
+  ]
 
   useEffect(() => {
     const trials = [
@@ -40,6 +68,18 @@ const Trials = () => {
     setTrials(trials);
   }, []);
 
+  const menuButtonStyle = {
+    color: 'black',
+  }
+  const subMenuTemplate = (rowData) => {
+    return (
+    <div className={styles.myHiddenText}>
+      <Button icon="pi pi-ellipsis-h" iconPos="right" className="p-button-text p-button-plain" style={ menuButtonStyle }
+          onClick={(event) => menu.current.toggle(event)} ></Button>
+    </div>
+    );
+  }
+
   return (
     <>
       <TopBar />
@@ -50,18 +90,22 @@ const Trials = () => {
             <Button label="Import" className="p-button-text p-button-plain" />
             <Button label="Create CTML" className={styles.createCtmlButton} onClick={(e) => createCtmlClick(e)} />
           </div>
-
         </div>
+
         <div className={styles.tableContainer}>
-          <DataTable value={trials}>
+          <DataTable value={trials} rowHover={true}>
             <Column field="id" header="ID"></Column>
+            <Column field="id" header="" body={subMenuTemplate}></Column>
             <Column field="nickname" header="Nickname"></Column>
-            <Column field="principalInvestigator" header="Principal Investigator"></Column>
+            <Column field="principalInvestigator" header="Principal Investigator" ></Column>
             <Column field="status" header="Status"></Column>
             <Column field="createdOn" header="Created on"></Column>
             <Column field="updatedOn" header="Modified on"></Column>
           </DataTable>
         </div>
+
+        <Menu model={trialMenuItems} ref={menu} popup id="popup_menu" className={styles.menu} appendTo={'self'}
+              onMouseLeave={(event) => menu.current.hide}/>
       </div>
     </>
 
