05e0d478f207cf8cdc0cd4267e3095b70a867fe9
Anton <anton.sukhovatkin@rmp.uhn.ca>
2022-11-27 18:58:30 -0500

    schema form for all basic fields

--- libs/ui/src/lib/custom-rjsf-templates/generatedSchema.ts
+++ libs/ui/src/lib/custom-rjsf-templates/generatedSchema.ts
@@ -8,9 +8,21 @@ export const schema = {
     "clinicalMetadata": {
       "type": "object",
       "properties": {
-        "disease_status": {
+        "nct_id": {
           "type": "string",
-          "title": "Disease Status"
+          "title": "NCT ID"
+        },
+        "long_title": {
+          "type": "string",
+          "title": "Long Title"
+        },
+        "short_title": {
+          "type": "string",
+          "title": "Short Title"
+        },
+        "age": {
+          "type": "string",
+          "title": "Age"
         },
         "nct_purpose": {
           "type": "string",
@@ -20,17 +32,13 @@ export const schema = {
           "type": "string",
           "title": "Phase"
         },
-        "prior_treatment_requirements": {
-          "type": "string",
-          "title": "Prior treatment requirements"
-        },
-        "protocol_number": {
+        "protocol_no": {
           "type": "string",
           "title": "Protocol Number"
         },
-        "short_title": {
+        "status": {
           "type": "string",
-          "title": "Short Title"
+          "title": "Status"
         }
       },
       "title": "Clinical Metadata"
@@ -53,18 +61,179 @@ export const schema = {
         }
       },
       "title": "Drug List"
+    },
+    "management_group_list": {
+      "type": "object",
+      "properties": {
+        "managementGroup": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "is_primary": {
+                "type": "string",
+                "enum": [
+                  "Y",
+                  "N"
+                ],
+                "title": "Is Primary"
+              },
+              "management_group_name": {
+                "type": "string",
+                "title": "Management Group Name"
+              }
+            }
+          },
+          "title": "Management Group"
+        }
+      },
+      "title": "Management Group List"
+    },
+    "site_list": {
+      "type": "object",
+      "properties": {
+        "site": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "coordinating_center": {
+                "type": "string",
+                "enum": [
+                  "Y",
+                  "N"
+                ],
+                "title": "Coordinating Center"
+              },
+              "uses_cancer_center_irb": {
+                "type": "string",
+                "enum": [
+                  "Y",
+                  "N"
+                ],
+                "title": "User Cancer Center IRB"
+              },
+              "site_name": {
+                "type": "string",
+                "title": "Site Name"
+              },
+              "site_status": {
+                "type": "string",
+                "title": "Site Status"
+              }
+            }
+          },
+          "title": "Site"
+        }
+      },
+      "title": "Site List"
+    },
+    "sponsor_list": {
+      "type": "object",
+      "properties": {
+        "site": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "is_principal_sponsor": {
+                "type": "string",
+                "enum": [
+                  "Y",
+                  "N"
+                ],
+                "title": "Is Principal Sponsor"
+              },
+              "sponsor_name": {
+                "type": "string",
+                "title": "Sponsor Name"
+              }
+            }
+          },
+          "title": "Sponsor"
+        }
+      },
+      "title": "Sponsor List"
+    },
+    "staff_list": {
+      "type": "object",
+      "properties": {
+        "protocol_staff": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "first_name": {
+                "type": "string",
+                "title": "First Name"
+              },
+              "last_name": {
+                "type": "string",
+                "title": "Last Name"
+              },
+              "email": {
+                "type": "string",
+                "title": "Email"
+              },
+              "institution_name": {
+                "type": "string",
+                "title": "Institution Name"
+              },
+              "staff_role": {
+                "type": "string",
+                "title": "Staff Role"
+              }
+            }
+          },
+          "title": "Protocol Staff"
+        }
+      },
+      "title": "Staff List"
+    },
+    "prior_treatment_requirements": {
+      "type": "array",
+      "title": "Prior treatment requirements",
+      "items": {
+        "type": "string"
+      }
     }
   }
 }
 export const uiSchema = {
-  // "ui:ObjectFieldTemplate": RjsfGridFieldTemplate,
   "ui:spacing": 16,
   "ui:layout": [
     {
       "clinicalMetadata": {
         "span": 24
-      },
+      }
+    },
+    {
       "drugList": {
+        "span": 24
+      }
+    },
+    {
+      "management_group_list": {
+        "span": 24
+      }
+    },
+    {
+      "site_list": {
+        "span": 24
+      }
+    },
+    {
+      "sponsor_list": {
+        "span": 24
+      }
+    },
+    {
+      "staff_list": {
+        "span": 24
+      }
+    },
+    {
+      "prior_treatment_requirements": {
         "span": 24
       }
     }
@@ -74,7 +243,22 @@ export const uiSchema = {
     "ui:spacing": 16,
     "ui:layout": [
       {
-        "disease_status": {
+        "nct_id": {
+          "span": 24
+        }
+      },
+      {
+        "long_title": {
+          "span": 24
+        }
+      },
+      {
+        "short_title": {
+          "span": 24
+        }
+      },
+      {
+        "age": {
           "span": 24
         }
       },
@@ -89,16 +273,13 @@ export const uiSchema = {
         }
       },
       {
-        "prior_treatment_requirements": {
+        "protocol_no": {
           "span": 24
         }
       },
       {
-        "protocol_number": {
-          "span": 12
-        },
-        "short_title": {
-          "span": 12
+        "status": {
+          "span": 24
         }
       }
     ]
@@ -128,6 +309,147 @@ export const uiSchema = {
             "ui:order": [
               "drug_name"
             ]
+          }
+        ]
+      }
+    }
+  },
+  "management_group_list": {
+    "ui:ObjectFieldTemplate": CtimsObjectFieldTemplate,
+    "ui:spacing": 16,
+    "ui:layout": [
+      {
+        "managementGroup": {
+          "span": 24
+        },
+        "ui:order": [
+          "managementGroup"
+        ]
+      }
+    ],
+    "managementGroup": {
+      "items": {
+        "ui:ObjectFieldTemplate": CtimsItemObjectFieldTemplate,
+        "ui:spacing": 16,
+        "ui:layout": [
+          {
+            "is_primary": {
+              "span": 12
+            },
+            "ui:order": [
+              "is_primary",
+              "management_group_name"
+            ],
+            "management_group_name": {
+              "span": 12
+            }
+          }
+        ]
+      }
+    }
+  },
+  "site_list": {
+    "ui:ObjectFieldTemplate": CtimsObjectFieldTemplate,
+    "ui:spacing": 16,
+    "ui:layout": [
+      {
+        "site": {
+          "span": 24
+        },
+        "ui:order": [
+          "site"
+        ]
+      }
+    ],
+    "site": {
+      "items": {
+        "ui:ObjectFieldTemplate": CtimsItemObjectFieldTemplate,
+        "ui:spacing": 16,
+        "ui:layout": [
+          {
+            "coordinating_center": {
+              "span": 12
+            },
+            "uses_cancer_center_irb": {
+              "span": 12
+            }
+          },
+          {
+            "site_name": {
+              "span": 12
+            },
+            "site_status": {
+              "span": 12
+            }
+          }
+        ]
+      }
+    }
+  },
+  "sponsor_list": {
+    "ui:ObjectFieldTemplate": CtimsObjectFieldTemplate,
+    "ui:spacing": 16,
+    "ui:layout": [
+      {
+        "site": {
+          "span": 24
+        }
+      }
+    ],
+    "site": {
+      "items": {
+        "ui:ObjectFieldTemplate": CtimsItemObjectFieldTemplate,
+        "ui:spacing": 16,
+        "ui:layout": [
+          {
+            "is_principal_sponsor": {
+              "span": 12
+            },
+            "sponsor_name": {
+              "span": 12
+            }
+          }
+        ]
+      }
+    }
+  },
+  "staff_list": {
+    "ui:ObjectFieldTemplate": CtimsObjectFieldTemplate,
+    "ui:spacing": 16,
+    "ui:layout": [
+      {
+        "protocol_staff": {
+          "span": 24
+        }
+      }
+    ],
+    "protocol_staff": {
+      "items": {
+        "ui:ObjectFieldTemplate": CtimsItemObjectFieldTemplate,
+        "ui:spacing": 16,
+        "ui:layout": [
+          {
+            "first_name": {
+              "span": 12
+            },
+            "last_name": {
+              "span": 12
+            }
+          },
+          {
+            "email": {
+              "span": 24
+            }
+          },
+          {
+            "institution_name": {
+              "span": 24
+            }
+          },
+          {
+            "staff_role": {
+              "span": 24
+            }
           }
         ]
       }
