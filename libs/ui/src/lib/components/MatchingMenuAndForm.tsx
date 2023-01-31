f047183eb2c490b32b0546f53ae4dbff3d33ca7c
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-01-31 13:15:46 -0500

    genomic and clinical form schemas minus repeating group at the end

--- libs/ui/src/lib/components/MatchingMenuAndForm.tsx
+++ libs/ui/src/lib/components/MatchingMenuAndForm.tsx
@@ -14,6 +14,7 @@ import {Button} from "primereact/button";
 import {withTheme} from "@rjsf/core";
 import {Theme as PrimeTheme} from "../primereact";
 import TreeNode from "primereact/treenode";
+import CtimsMatchDialogObjectFieldTemplate from "../custom-rjsf-templates/CtimsMatchDialogObjectFieldTemplate";
 
 const Form = withTheme(PrimeTheme)
 
@@ -29,6 +30,18 @@ export interface IRootNode {
 interface IComponentType {
   type: EComponentType;
   node: TreeNode;
+}
+
+const widgets: RegistryWidgetsType = {
+  TextWidget: CtimsInput,
+  SelectWidget: CtimsDropdown
+}
+
+const formContainerStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  overflowY: 'scroll'
 }
 
 const OperatorDropdown = () => {
@@ -92,7 +105,6 @@ const MatchingMenuAndForm = (props: any) => {
   const [componentType, setComponentType] = useState<IComponentType>({type: EComponentType.None, node: {}});
   const [isEmpty, setIsEmpty] = useState(true);
   const [buildRootNodeParams, setBuildRootNodeParams] = useState<IRootNode>({rootLabel: '', firstChildLabel: ''});
-  const [isMouseOverNode, setIsMouseOverNode] = useState(false);
 
   const menu = useRef(null);
 
@@ -112,29 +124,43 @@ const MatchingMenuAndForm = (props: any) => {
 
   const ClinicalForm = (props: IFormProps) => {
     const {node} = props
-    console.log('ClinicalForm node: ', node)
-
-    const widgets: RegistryWidgetsType = {
-      TextWidget: CtimsInput,
-      SelectWidget: CtimsDropdown
-    }
+    // console.log('ClinicalForm node: ', node)
 
     const clinicalFormSchema = {
       'type': 'object',
-      'required': ['age', 'oncotreePrimaryDiagnosis'],
+      'required': ['age_numerical', 'oncotree_primary_diagnosis'],
       'properties': {
-        'age': {
+        'age_numerical': {
           'type': 'string',
           'title': 'Age',
         },
-        oncotreePrimaryDiagnosis: {
+        'oncotree_primary_diagnosis': {
           'type': 'string',
           'title': 'Oncotree Primary Diagnosis',
+        },
+        'tmb': {
+          'type': 'string',
+          'title': 'TMB',
+        },
+        'her2_status': {
+          'type': 'string',
+          'title': 'HER2 Status',
+        },
+        'er_status': {
+          'type': 'string',
+          'title': 'ER Status',
+        },
+        'pr_status': {
+          'type': 'string',
+          'title': 'PR Status',
         }
       }
     }
     const clinicalUiSchema = {
-      "ui:ObjectFieldTemplate": CtimsObjectFieldTemplate
+      "ui:ObjectFieldTemplate": CtimsMatchDialogObjectFieldTemplate,
+      "ui:submitButtonOptions": {
+        "norender": true,
+      },
     }
 
     const onFormChange = (data: any) => {
@@ -143,7 +169,7 @@ const MatchingMenuAndForm = (props: any) => {
     }
 
     return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <div style={formContainerStyle}>
         <OperatorDropdown />
         <div>
           <TitleContainer title="Clinical" />
@@ -161,15 +187,384 @@ const MatchingMenuAndForm = (props: any) => {
   }
 
   const GenomicForm = (props: IFormProps) => {
-    // console.log('GenomicForm rootNodes: ', rootNodes)
-    return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <OperatorDropdown />
-        <div>
-          <TitleContainer title="Genomic" />
-        </div>
+    const {node} = props
+
+  const genomicFormSchema = {
+        'definitions': {
+          'variant_category': {
+            "enumNames": [
+              "Missense Mutation",
+              "Missense and Splice Region",
+              "Del Ins",
+              "Frame Shift Del",
+              "Frame Shift Ins",
+              "Frameshift",
+              "Frameshift mutation",
+              "In Frame Del",
+              "In Frame Ins",
+              "Initiator Codon",
+              "Intron mutation",
+              "Intron",
+              "3'UTR",
+              "3_prime_UTR",
+              "5'Flank",
+              "5'UTR",
+              "5'UTR_mutation",
+              "5_prime_UTR",
+            ],
+            "enum": [
+              {
+                "label": "Missense Mutation",
+                "value": "Missense_Mutation",
+              },
+              {
+                "label": "Missense and Splice Region",
+                "value": "Missense and Splice_Region",
+              },
+              {
+                "label": "Del Ins",
+                "value": "Del_Ins",
+              },
+              {
+                "label": "Frame Shift Del",
+                "value": "Frame_Shift_Del",
+              },
+              {
+                "label": "Frame Shift Ins",
+                "value": "Frame_Shift_Ins",
+              },
+              {
+                "label": "Frameshift",
+                "value": "Frameshift",
+              },
+              {
+                "label": "Frameshift mutation",
+                "value": "Frameshift_mutation",
+              },
+              {
+                "label": "In Frame Del",
+                "value": "In_Frame_Del",
+              },
+              {
+                "label": "In Frame Ins",
+                "value": "In_Frame_Ins",
+              },
+              {
+                "label": "Initiator Codon",
+                "value": "Initiator_Codon",
+              },
+              {
+                "label": "Intron mutation",
+                "value": "Intron_mutation",
+              },
+              {
+                "label": "Intron",
+                "value": "Intron",
+              },
+              {
+                "label": "3'UTR",
+                "value": "3'UTR",
+              },
+              {
+                "label": "3_prime_UTR",
+                "value": "3_prime_UTR",
+              },
+              {
+                "label": "5'Flank",
+                "value": "5'Flank",
+              },
+              {
+                "label": "5'UTR",
+                "value": "5'UTR",
+              },
+              {
+                "label": "5'UTR_mutation",
+                "value": "5'UTR_mutation",
+              },
+              {
+                "label": "5_prime_UTR",
+                "value": "5_prime_UTR",
+              }
+            ]
+          },
+          'cnv_call': {
+            "enumNames": [
+              "Heterozygous deletion",
+              "Homozygous deletion",
+              "Gain",
+              "High level amplification",
+            ],
+            "enum": [
+              {
+                "label": "Heterozygous deletion",
+                "value": "Heterozygous_deletion",
+              },
+              {
+                "label": "Homozygous deletion",
+                "value": "Homozygous_deletion",
+              },
+              {
+                "label": "Gain",
+                "value": "Gain",
+              },
+              {
+                "label": "High level amplification",
+                "value": "High_level_amplification",
+              }
+            ]
+          },
+          'wildtype': {
+            "enumNames": [
+              "True",
+              "False",
+            ],
+            "enum": [
+              {
+                "label": "True",
+                "value": "true",
+              },
+              {
+                "label": "False",
+                "value": "false",
+              }
+            ]
+          },
+          'pole_status': {
+            "enumNames": [
+              "Yes",
+              "No",
+              "Cannot Access",
+              "Insufficient Variants",
+            ],
+            "enum": [
+              {
+                "label": "Yes",
+                "value": "yes",
+              },
+              {
+                "label": "No",
+                "value": "no",
+              },
+              {
+                "label": "Cannot Access",
+                "value": "cannot_access",
+              },
+              {
+                "label": "Insufficient Variants",
+                "value": "insufficient_variants",
+              }
+            ],
+          },
+          'uva_status': {
+            "enumNames": [
+              "Yes",
+              "No",
+              "Cannot Access",
+              "Insufficient Variants",
+            ],
+            "enum": [
+              {
+                "label": "Yes",
+                "value": "yes",
+              },
+              {
+                "label": "No",
+                "value": "no",
+              },
+              {
+                "label": "Cannot Access",
+                "value": "cannot_access",
+              },
+              {
+                "label": "Insufficient Variants",
+                "value": "insufficient_variants",
+              }
+            ],
+          },
+          'tobacco_status': {
+            "enumNames": [
+              "Yes",
+              "No",
+              "Cannot Access",
+              "Insufficient Variants",
+            ],
+            "enum": [
+              {
+                "label": "Yes",
+                "value": "yes",
+              },
+              {
+                "label": "No",
+                "value": "no",
+              },
+              {
+                "label": "Cannot Access",
+                "value": "cannot_access",
+              },
+              {
+                "label": "Insufficient Variants",
+                "value": "insufficient_variants",
+              }
+            ],
+          },
+          'apobec_status': {
+            "enumNames": [
+              "Yes",
+              "No",
+              "Cannot Access",
+              "Insufficient Variants",
+            ],
+            "enum": [
+              {
+                "label": "Yes",
+                "value": "yes",
+              },
+              {
+                "label": "No",
+                "value": "no",
+              },
+              {
+                "label": "Cannot Access",
+                "value": "cannot_access",
+              },
+              {
+                "label": "Insufficient Variants",
+                "value": "insufficient_variants",
+              }
+            ],
+          },
+          'temozolomide_status': {
+            "enumNames": [
+              "Yes",
+              "No",
+              "Cannot Access",
+              "Insufficient Variants",
+            ],
+            "enum": [
+              {
+                "label": "Yes",
+                "value": "yes",
+              },
+              {
+                "label": "No",
+                "value": "no",
+              },
+              {
+                "label": "Cannot Access",
+                "value": "cannot_access",
+              },
+              {
+                "label": "Insufficient Variants",
+                "value": "insufficient_variants",
+              }
+            ],
+          },
+          'mmr_status': {
+            "enumNames": [
+              "MMR-Proficient",
+              "MMR-Deficient"
+            ],
+            "enum": [
+              {
+                "label": "MMR-Proficient",
+                "value": "mmr_proficient",
+              },
+              {
+                "label": "MMR-Deficient",
+                "value": "mmr_deficient",
+              }
+            ]
+          }
+        },
+        'type': 'object',
+        'required': ['hugo_symbol', 'variant_category'],
+        'properties': {
+          'hugo_symbol': {
+            'type': 'string',
+            'title': 'Hugo Symbol',
+          },
+          'variant_category': {
+            "$ref": "#/definitions/variant_category",
+            'title': 'Variant Category',
+          },
+          'protein_change': {
+            'type': 'string',
+            'title': 'Protein Change',
+          },
+          'variant_classification': {
+            'type': 'string',
+            'title': 'Variant Classification',
+          },
+          'cnv_call': {
+            'title': 'CNV Call',
+            '$ref': '#/definitions/cnv_call',
+          },
+          'fusion_partner_hugo_symbol': {
+            'type': 'string',
+            'title': 'Fusion Partner Hugo Symbol',
+          },
+          'true_transcript_exon': {
+            'type': 'string',
+            'title': 'True Transcript Exon',
+          },
+          'wildtype': {
+            'title': 'Wildtype',
+            '$ref': '#/definitions/wildtype',
+          },
+          'pole_status': {
+            'title': 'POLE Status',
+            '$ref': '#/definitions/pole_status',
+          },
+          'uva_status': {
+            'title': 'UVA Status',
+            '$ref': '#/definitions/uva_status',
+          },
+          'tobacco_status': {
+            'title': 'Tobacco Status',
+            '$ref': '#/definitions/tobacco_status',
+          },
+          'apobec_status': {
+            'title': 'APOBEC Status',
+            '$ref': '#/definitions/apobec_status',
+          },
+          'temozolomide_status': {
+            'title': 'Temozolomide Status',
+            '$ref': '#/definitions/temozolomide_status',
+          },
+          'mmr_status': {
+            'title': 'MMR Status',
+            '$ref': '#/definitions/mmr_status',
+          }
+        }
+  };
+  const genomicUiSchema = {
+    "ui:ObjectFieldTemplate": CtimsMatchDialogObjectFieldTemplate,
+    "ui:submitButtonOptions": {
+      "norender": true,
+    },
+  }
+
+  const onFormChange = (data: any) => {
+    node.data.formData = data.formData;
+    console.log('onFormChange node: ', node)
+  }
+
+  return (
+    <div style={formContainerStyle}>
+      <OperatorDropdown />
+      <div>
+        <TitleContainer title="Genomic" />
       </div>
-    )
+      <div>
+        <Form schema={genomicFormSchema as JSONSchema7}
+              formData={node.data.formData}
+              uiSchema={genomicUiSchema}
+              widgets={widgets}
+              onChange={onFormChange}
+              validator={localValidator}/>
+      </div>
+    </div>
+  )
   }
 
   const EmptyForm = () => {
---
+++ libs/ui/src/lib/custom-rjsf-templates/CtimsMatchDialogObjectFieldTemplate.tsx
@@ -0,0 +1,106 @@
+import {getTemplate, getUiOptions, ObjectFieldTemplatePropertyType, ObjectFieldTemplateProps} from "@rjsf/utils";
+import React from "react";
+
+const CtimsMatchDialogObjectFieldTemplate = (props: ObjectFieldTemplateProps) => {
+  const {
+    description,
+    disabled,
+    formContext,
+    formData,
+    idSchema,
+    onAddClick,
+    properties,
+    readonly,
+    required,
+    registry,
+    schema,
+    title,
+    uiSchema,
+  } = props;
+
+  const uiOptions = getUiOptions(uiSchema);
+  const TitleFieldTemplate = getTemplate<"TitleFieldTemplate">(
+    "TitleFieldTemplate",
+    registry,
+    uiOptions
+  );
+  const DescriptionFieldTemplate = getTemplate<"DescriptionFieldTemplate">(
+    "DescriptionFieldTemplate",
+    registry,
+    uiOptions
+  );
+
+  const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: '20px',
+    marginRight: '20px',
+    marginBottom: '20px',
+  }
+
+  const titleDescriptionStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+
+
+  }
+  const arrayContainer: React.CSSProperties = {
+    width: '640px',
+    marginLeft: 'auto',
+  }
+
+  const hrStyle: React.CSSProperties = {
+    marginBottom: '20px',
+    marginTop: '20px',
+    border: '1px solid #e0e0e0',
+    backgroundColor: '#e0e0e0',
+  }
+
+  // console.log('schema', schema)
+  // console.log('uiSchema', uiSchema)
+  // console.log('title', title)
+
+  return (
+    <>
+      {/*<hr style={hrStyle}/>*/}
+      <div style={containerStyle}>
+        <div style={titleDescriptionStyle}>
+          {(schema.title) && (
+            <TitleFieldTemplate
+              id={`${idSchema.$id}-title`}
+              title={schema.title}
+              required={required}
+              schema={schema}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          )}
+          {(uiOptions.description || description) && (
+            <DescriptionFieldTemplate
+              id={`${idSchema.$id}-description`}
+              description={uiOptions.description || description!}
+              schema={schema}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          )}
+        </div>
+
+        <div id={`object-field-template-${idSchema.$id}`} style={arrayContainer}>
+          {properties
+            .filter((e) => !e.hidden)
+            .map((element: ObjectFieldTemplatePropertyType, index: number) => (
+              <div
+                key={index}
+                className={element.hidden ? "d-none" : undefined}
+              >
+                {element.content}
+              </div>
+            ))}
+        </div>
+      </div>
+      {/*<hr style={hrStyle}/>*/}
+    </>
+  );
+}
+export default CtimsMatchDialogObjectFieldTemplate
