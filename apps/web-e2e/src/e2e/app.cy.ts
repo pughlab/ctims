76648788d6495ee19987f9cddb4a1739e28fb0c1
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-03-11 11:15:12 -0500

    cypress setup done

--- apps/web-e2e/src/e2e/app.cy.ts
+++ apps/web-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,15 @@
-import { getGreeting } from '../support/app.po';
+import {getPrincipalInvestigator, getTrialId, getTrialNickname, selectDraftCtmlStatus} from '../support/app.po';
 
 describe('web', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome web');
+    cy.pause();
+    getTrialId().type('ABC-123');
+    getTrialNickname().type('My Trial');
+    getPrincipalInvestigator().type('Dr. John Doe');
+    selectDraftCtmlStatus();
   });
 });
