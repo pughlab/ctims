952569086238244bd29b971de7ecb38553409062
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-03-11 11:25:03 -0500

    cypress cleanup

--- apps/web-e2e/src/support/app.po.ts
+++ apps/web-e2e/src/support/app.po.ts
@@ -1,12 +1,11 @@
-export const getGreeting = () => cy.get('h1');
 export const getTrialId = () => cy.get('#root_trialInformation_trial_id');
 export const getTrialNickname = () => cy.get('#root_trialInformation_nickname');
 export const getPrincipalInvestigator = () => cy.get('#root_trialInformation_principal_investigator')
 
 
 //Dropdown button for Ctml Status
-export const clickCtmlStatusDropdown = () => cy.get('#root_trialInformation_ctml_status > .p-dropdown-trigger').click();
-export const selectCtmlStatus = () => cy.get('[aria-label="Draft"]').click();
+const clickCtmlStatusDropdown = () => cy.get('#root_trialInformation_ctml_status > .p-dropdown-trigger').click();
+const selectCtmlStatus = () => cy.get('[aria-label="Draft"]').click();
 
 export const selectDraftCtmlStatus = () => {
   clickCtmlStatusDropdown();
