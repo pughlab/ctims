9935c0a97d701748d72d879a05a40a859d62ec0f
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-03-15 11:44:42 -0400

    Trial Editor Form Validation

--- apps/web-e2e/src/e2e/TrialEditorTest.cy.ts
+++ apps/web-e2e/src/e2e/TrialEditorTest.cy.ts
@@ -10,16 +10,10 @@ import {
   getCheckBoxArmIsSuspended,
   getCheckBoxCancerCenterIRB,
   getCheckBoxCoordinateCenter,
-  getCheckBoxIcon,
-  getCheckBoxLevelIsSuspended,
+  getCheckBoxIcon, getCheckBoxLevelIsSuspended,
   getCheckBoxPrimaryManagementGroup,
   getCheckBoxPrincipalSponsor,
   getClickPhase,
-  getClinicalAge, getClinicalDropdown,
-  getClinicalERStatus,
-  getClinicalHER2Status,
-  getClinicalOncotreePrimaryDiagnosis,
-  getClinicalPRStatus,
   getCtmlStatusDropdown,
   getDefaultTextMatchingCriteria,
   getDrugName,
@@ -33,9 +27,7 @@ import {
   getLongTitle,
   getManagementGroupName,
   getMenuItemAnd,
-  getMenuItemClinical,
   getMenuItemClinicalGenomic,
-  getMenuItemOr,
   getNCTPurpose,
   getPhaseDropdownList,
   getPlusIcon,
@@ -67,58 +59,95 @@ describe('CTIMS Trial Editor', () => {
   it('should Validate the Trial Editor Page', () => {
     cy.title().should('contain','CTIMS')
     trialEditorLeftPanelList().should('have.length','8')
-   /* cy.trialInformation(NCT04293094_testData.nct_id,
-      "My Trial",
-      "John Doe",
-      "Draft",
-      NCT04293094_testData.long_title,
-      NCT04293094_testData.short_title,
-      NCT04293094_testData.phase,
-      NCT04293094_testData.protocol_no,
-      NCT04293094_testData.nct_purpose,
-      NCT04293094_testData.status)
-
+    trialEditorLeftPanelList().eq(0).should('contain','Trial Information').click()
+    getTrialId().clear().type(NCT04293094_testData.nct_id);
+    getTrialNickname().clear().type('My Trial');
+    getPrincipalInvestigator().clear().type('Dr. John Doe');
+    //ctml status
+    getCtmlStatusDropdown().click()
+    cy.wait(1000)
+    getPhaseDropdownList().contains('Draft').click()
+    getLongTitle().clear().type(NCT04293094_testData.long_title)
+    getShortTitle().clear().type(NCT04293094_testData.short_title)
+    //Phase
+    getClickPhase().click()
+    getPhaseDropdownList().contains(NCT04293094_testData.phase).click()
+    getProtocolNumber().clear().type(String(NCT04293094_testData.protocol_no))
+    getNCTPurpose().clear().type(NCT04293094_testData.nct_purpose)
+    //getPlusIcon().eq(0).click() //at index 0 "Add drug" plus icon is present
     //Age
-    cy.age(NCT04293094_testData.age)
+    trialEditorLeftPanelList().eq(1).should('contain','Age').click()
+    getAgeGroup().type(NCT04293094_testData.age)
 
     //Drug List
-    cy.drugList(NCT04293094_testData.drug_list.drug[0].drug_name)
+    trialEditorLeftPanelList().eq(2).should('contain','Drug List').click()
+    getDrugName().type(NCT04293094_testData.drug_list.drug[0].drug_name)
 
-    //Management Group List has 1-text field and 1-Checkbox
-    cy.managementGroupList(NCT04293094_testData.management_group_list.management_group[0].management_group_name,
-      NCT04293094_testData.management_group_list.management_group[0].is_primary)
-*/
-    //Site List has 2-Text field and 2-checkbox
-    cy.siteList(NCT04293094_testData.site_list.site[0].site_name,
-      NCT04293094_testData.site_list.site[0].site_status,
-      NCT04293094_testData.site_list.site[0].coordinating_center,
-      NCT04293094_testData.site_list.site[0].uses_cancer_center_irb)
-
+    //Management Group List
+    trialEditorLeftPanelList().eq(3).should('contain','Management Group List').click()
+    getManagementGroupName().type(NCT04293094_testData.management_group_list.management_group[0].management_group_name)
+    if(NCT04293094_testData.management_group_list.management_group[0].is_primary === 'Y') {
+      getCheckBoxPrimaryManagementGroup().click().should('have.class','p-checkbox-checked') //This is a primary
+      // management group
+    } else {
+      getCheckBoxPrimaryManagementGroup().should('have.class','p-checkbox') //This is a primary management group
+    }
+      //Site List
+    trialEditorLeftPanelList().eq(4).should('contain','Site List').click()
+    getSiteName().type(NCT04293094_testData.site_list.site[0].site_name)
+    getSiteStatus().type(NCT04293094_testData.site_list.site[0].site_status)
+    if(NCT04293094_testData.site_list.site[0].coordinating_center === 'Y') {
+      getCheckBoxCoordinateCenter().click().should('have.class','p-checkbox-checked') //This site is a coordinating center.
+    } else {
+      getCheckBoxCoordinateCenter().eq(1).should('have.class','p-checkbox')
+    }
+    if(NCT04293094_testData.site_list.site[0].uses_cancer_center_irb === 'Y') {
+      getCheckBoxCancerCenterIRB().click().should('have.class','p-checkbox-checked') //This site uses cancer center IRB.
+    } else{
+      getCheckBoxCancerCenterIRB().should('have.class','p-checkbox')
+    }
     //Sponsor List
-    cy.sponsorList(NCT04293094_testData.sponsor_list.sponsor[0].sponsor_name,
-      NCT04293094_testData.sponsor_list.sponsor[0].is_principal_sponsor)
-
+    trialEditorLeftPanelList().eq(5).should('contain','Sponsor List').click()
+    getSponsorName().type(NCT04293094_testData.sponsor_list.sponsor[0].sponsor_name)
+    if(NCT04293094_testData.sponsor_list.sponsor[0].is_principal_sponsor === 'Y') {
+      getCheckBoxPrincipalSponsor().click().should('have.class','p-checkbox-checked') //This sponsor is a principal sponsor.
+    } else {
+      getCheckBoxPrincipalSponsor().should('have.class','p-checkbox')
+    }
     //Staff List
-    cy.staffList(NCT04293094_testData.staff_list.protocol_staff[0].first_name,
-      NCT04293094_testData.staff_list.protocol_staff[0].last_name,
-      NCT04293094_testData.staff_list.protocol_staff[0].email_address,
-      NCT04293094_testData.staff_list.protocol_staff[0].institution_name,
-      NCT04293094_testData.staff_list.protocol_staff[0].staff_role)
+    trialEditorLeftPanelList().eq(6).should('contain','Staff List').click()
+    getProtocolStaffFirstName().type(NCT04293094_testData.staff_list.protocol_staff[0].first_name)
+    getProtocolStaffLastName().type(NCT04293094_testData.staff_list.protocol_staff[0].last_name)
+    getProtocolStaffEmail().type(NCT04293094_testData.staff_list.protocol_staff[0].email_address)
+    getProtocolStaffInstitutionalName().type(NCT04293094_testData.staff_list.protocol_staff[0].institution_name)
+    getProtocolStaffRole().type(NCT04293094_testData.staff_list.protocol_staff[0].staff_role)
+    //status not given in json data
+    //getProtocolStaffStatus().type(NCT04293094_testData.staff_list.protocol_staff[0].status)
 
     //Arm code
-    cy.arm(NCT04293094_testData.treatment_list.step[0].arm[0].arm_code,
-      NCT04293094_testData.treatment_list.step[0].arm[0].arm_description,
-      NCT04293094_testData.treatment_list.step[0].arm[0].arm_internal_id,
-      NCT04293094_testData.treatment_list.step[0].arm[0].arm_suspended)
-
+    getArmCode().type(String(NCT04293094_testData.treatment_list.step[0].arm[0].arm_code))
+    getArmDescription().type(NCT04293094_testData.treatment_list.step[0].arm[0].arm_description)
+    getArmInternalId().type(String(NCT04293094_testData.treatment_list.step[0].arm[0].arm_internal_id))
+    const arm_suspended = NCT04293094_testData.treatment_list.step[0].arm[0].arm_suspended
+    if(arm_suspended === 'Y') {
+      getCheckBoxArmIsSuspended().click().should('have.class','p-checkbox-checked')
+    } else if (arm_suspended === 'N'){
+      getCheckBoxArmIsSuspended().should('have.class','p-checkbox')
+    }
     //Level code
-    cy.doseLevel(NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_code,
-      NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_description,
-      NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_internal_id.toString(),
-      NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_suspended)
-    //click Match criteria
+    getLevelCode().type(NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_code)
+    const levelCode = NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_code;
+    cy.log(levelCode);
+    getLevelDescription().type(NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_description)
+    getLevelInternalId().type(String(NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_internal_id))
+    const level_suspended = NCT04293094_testData.treatment_list.step[0].arm[0].dose_level[0].level_code
+    if( level_suspended === 'Y') {
+      getCheckBoxLevelIsSuspended().click().should('have.class','p-checkbox-checked')
+    }
+    else if(level_suspended === 'N') {
+      getCheckBoxLevelIsSuspended().should('have.class','p-checkbox')
+    }
     getEditMatchingCriteria().click()
-
     getDefaultTextMatchingCriteria().should('contain','Matching criteria inputs will be shown here.')
     getAddCriteriaGroup().click()
     getLeftMenuComponent().find('span').contains('And')
@@ -137,8 +166,10 @@ describe('CTIMS Trial Editor', () => {
     getMenuItemClinicalGenomic().eq(1).should('contain','Genomic').click()
     //Now Genomic subtree is created, click on the sub tree "Genomic" at index 1
     getLeftMenuComponent().eq(1).click()
+    // @ts-ignore
     getHugoSymbol().type(NCT04293094_testData.treatment_list.step[0].arm[0].match[0].or[0].genomic.hugo_symbol)
     getVariantCategory().click() //has dropdown
+    // @ts-ignore //included this sine "or" is not recognized
     const variant_category = NCT04293094_testData.treatment_list.step[0].arm[0].match[0].or[0].genomic.variant_category
    cy.log(variant_category)
     if(variant_category === 'Mutation') {
@@ -157,71 +188,25 @@ describe('CTIMS Trial Editor', () => {
       getGenomicDropDown().contains('Signature').click()
       getVariantCategory().should('contain','Signature')
     }
-    //click on index 0 at first child to add a sub group
+    //click oon index 0 at first child to add a sub group
     getLeftMenuComponent().eq(0).trigger('mouseover').click()
     cy.wait(1000)
     getTruncateButton().should('be.visible').click()
-    getAddCriteriaToSubGroup().should('contain','Add criteria subgroup').trigger('mouseover')
+    getAddCriteriaToSubGroup().should('contain','Add criteria subgroup').trigger('mouseover').then(() => {
       cy.wait(1000)
       //.next().trigger('mouseover').click()
       //cy.get('.p-menuitem-active > .p-submenu-list > :nth-child(1) > .p-menuitem-link').click()
       //cy.wait(1000)
       getMenuItemAnd().trigger('mouseover').click({force: true})
-      // getMenuItemAnd().trigger('mouseover').click({force: true})
-      //getMenuItemAnd().invoke('addClass', 'p-menuitem-active').click({force: true})
-    getAddCriteriaToSubGroup().invoke('addClass', 'p-menuitem-active')
-    getMenuItemAnd().click()
-    //click on the second child component "And" to add clinical data
-    getLeftMenuComponent().eq(2).trigger('mouseover').click() //.invoke('addClass','p-button-icon-only')
-    //cy.wait(1000)
-    getTruncateButton().should('be.visible').click()
-    getAddCriteriaToSameGroup().trigger('mouseover').invoke('addClass', 'p-menuitem-active')
-    getMenuItemClinical().click()
-    //expand the tree at index 2
-    cy.get(':nth-child(2) > .p-treenode-content > .p-tree-toggler').click()
-    //click the 3rd child component Clinical
-    getLeftMenuComponent().eq(3).click()
-    //Enter Age and OncoTree input value
-    getClinicalAge().type(NCT04293094_testData.treatment_list.step[0].arm[0].match[0].or[1].and[0].clinical.age_numerical)
-    getClinicalOncotreePrimaryDiagnosis().type(NCT04293094_testData.treatment_list.step[0].arm[0].match[0].or[1].and[0].clinical.oncotree_primary_diagnosis)
+    })
+    //cy.get('.p-submenu-list>li>a>.p-menuitem-text').contains('And (if all criteria are met)').click()
+   // cy.wait(1000)
+    //getMenuItemAnd().click()
+    //.should('be.visible')
+   // cy.wait(1000)
 
-    //Second Add
-    getLeftMenuComponent().eq(2).trigger('mouseover').click() //.invoke('addClass','p-button-icon-only')
-    getTruncateButton().should('be.visible').click()
-    getAddCriteriaToSubGroup().trigger('mouseover').invoke('addClass', 'p-menuitem-active')
-    getMenuItemAnd().click()
+    //.click()
 
-    //Add "or" child of the above "And at index 4"
-    getLeftMenuComponent().eq(4).trigger('mouseover').click() //.invoke('addClass','p-button-icon-only')
-    getTruncateButton().should('be.visible').click()
-    getAddCriteriaToSubGroup().trigger('mouseover').invoke('addClass', 'p-menuitem-active')
-    getMenuItemOr().click()
 
-    //click on the "or" toggle arrow button to add three clinical data
-    cy.get(':nth-child(2) > .p-treenode-children > :nth-child(2) > .p-treenode-content > .p-tree-toggler > .p-tree-toggler-icon').click()
-    getLeftMenuComponent().eq(5).trigger('mouseover').click()
-    getTruncateButton().should('be.visible').click()
-    getAddCriteriaToSameGroup().trigger('mouseover').invoke('addClass', 'p-menuitem-active')
-    getMenuItemClinical().click()
-    //click on toggle arrow button to expand tree
-    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > .p-treenode-children > .p-treenode > .p-treenode-content' +
-      ' > .p-tree-toggler').click()
-    //click clinical item to add data
-    getLeftMenuComponent().eq(6).click()
-    getClinicalAge().type(NCT04293094_testData.treatment_list.step[0].arm[0].match[1].and[0].or[0].clinical.age_numerical)
-    getClinicalOncotreePrimaryDiagnosis().type(NCT04293094_testData.treatment_list.step[0].arm[0].match[1].and[0].or[0].clinical.oncotree_primary_diagnosis)
-    // @ts-ignore
-   /* let her2_status = NCT04293094_testData.treatment_list.step[0].arm[0].match[1].and[0].or[0].her2_status
-    if( her2_status.boolean === true) {
-      getClinicalHER2Status().should('not.be.selected')
-    } else if(her2_status.boolean === false) {
-      getClinicalHER2Status().click()
-        //.should('not.be.selected')
-      getClinicalDropdown().eq(1).click()
-    }*/
-    /*getClinicalHER2Status()
-    getClinicalERStatus()
-    getClinicalPRStatus()
-*/
   });
 });
