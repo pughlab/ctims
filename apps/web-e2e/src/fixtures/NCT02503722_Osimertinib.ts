b12bf8213dd5700839da7b3ac9f9d40349187afe
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-03-23 18:41:33 -0400

    Changed NCT02503722_Osimertinib.js file to typescript file, added interface for this file

---
+++ apps/web-e2e/src/fixtures/NCT02503722_Osimertinib.ts
@@ -0,0 +1,170 @@
+//import {Ctml2} from "../support/interfaces/CtmlInterfaces2";
+
+import {Ctml2} from "../support/interfaces/CtmlInterfaces2";
+
+export const NCT02503722_Osimertinib: Ctml2 = {
+  "age": "Adult",
+  "drug_list": {
+  "drug": [
+    {
+      "drug_name": "Osimertinib"
+    },
+    {
+      "drug_name": "Sapanisertib"
+    }
+  ]
+},
+  "last_updated": "September 2, 2021",
+  "long_title": "A Phase 1 Trial of MLN0128 (TAK-228) in Combination With Osimertinib (AZD9291) in Advanced EGFR Mutation Positive Non-Small Cell Lung Cancer (NSCLC) After Progression on a Previous EGFR Tyrosine Kinase Inhibitor",
+  "management_group_list": {
+  "management_group": [
+    {
+      "is_primary": "Y",
+      "management_group_name": "Princess Margaret Cancer Centre"
+    },
+    {
+      "is_primary": "Y",
+      "management_group_name": "BCCA-Vancouver Cancer Centre"
+    }
+  ]
+},
+  "match": [
+  {
+    "clinical": {
+      "disease_status": [
+        "Metastatic Lung Non-Small Cell Carcinoma",
+        "Recurrent Lung Non-Small Cell Carcinoma",
+        "Stage III Lung Non-Small Cell Cancer AJCC v7",
+        "Stage IIIA Lung Non-Small Cell Cancer AJCC v7",
+        "Stage IIIB Lung Non-Small Cell Cancer AJCC v7",
+        "Stage IV Lung Non-Small Cell Cancer AJCC v7"
+      ]
+    }
+  }
+],
+  "nct_id": "NCT02503722",
+  "nct_purpose": "This phase I trial studies the side effects and best dose of sapanisertib when given together with osimertinib in treating patients with stage IV EGFR mutation positive non-small cell lung cancer that has progressed after treatment with an EGFR tyrosine kinase inhibitor. Sapanisertib and osimertinib may stop the growth of tumor cells by blocking some of the enzymes needed for cell growth.",
+  "phase": "I",
+  "prior_treatment_requirements": [
+  "Patients with stage IV or recurrent/metastatic histologically or cytologically confirmed non-squamous NSCLC.",
+  "NSCLC must harbor an EGFR activating mutation (Exon 21 L858R, Exon 19 deletion).",
+  "Progressive disease on osimertinib (AZD9291) given first line.",
+  "For the dose expansion portion ONLY, patient must: 1) have progression of disease with first line osimertinib administered for advanced or metastatic disease as the last previous systemic treatment, 2) be treatment naïve for other 3rd generation EGFR-TKI (CO-1686) and mTOR inhibitors.",
+  "Patients must have measurable disease by Response Evaluation Criteria in Solid Tumors (RECIST) 1.1, defined as at least one lesion that can be accurately measured in at least one dimension >= 10 mm (>= 1 cm) by computed tomography (CT) imaging or magnetic resonance imaging (MRI) within 28 days prior to start of protocol therapy; the CT from a combined positron emission tomography (PET)/CT may be used if it is of diagnostic quality; laboratory parameters are not acceptable as the only evidence of disease.",
+  "For the dose expansion, no other systemic therapies for advanced/metastatic disease is permissible after first line osimertinib.",
+  "Eastern Cooperative Oncology Group (ECOG) performance status =< 1 (Karnofsky >= 70%).",
+  "Patients with a prior history of brain metastases are eligible provided: - The brain metastases have been treated; patients with small brain metastases for which radiation or surgery is not indicated, may be eligible on discussion with the study chair. Brain metastases that were managed with first line osimertinib is permissible, provided that the brain metastases are stable on a baseline MRI and for whom radiation or surgical resection is not indicated. - The patient is asymptomatic from the brain metastases. - Corticosteroids and anti-epileptic drugs prescribed for the management of brain metastases have been discontinued at least 7 days prior to registration. - The brain metastases are stable on pre-registration imaging.",
+  "Absolute neutrophil count (ANC) >= 1.5 x 10^9/L.",
+  "Hemoglobin >= 90 g/L (or >= 9 g/dL).",
+  "Platelets >= 100 x 10^9/L.",
+  "Calculated creatinine clearance of > 50 mL/min using Cockcroft Gault equation or 24-hour urine sampling.",
+  "Total bilirubin =< 1.5 institutional upper limit of normal.",
+  "Aspartate transaminase (AST) (serum glutamic-oxaloacetic transaminase [SGOT]) and alanine aminotransferase (ALT) (serum glutamate pyruvate transaminase [SGPT]) =< 2.5 x institutional upper limit of normal.",
+  "Fasting plasma glucose =< 130 mg/dL (or 7.2 mmol/L).",
+  "Glycosylated hemoglobin (Hb A1c) =< 7.0%.",
+  "Fasting triglycerides =< 300 mg/dL (3.42 mmol/L).",
+  "Cholesterol =< 300 mg/dL (7.75 mmol/L).",
+  "Fridericia's correction formula (QTcF) =< 470 msec.",
+  "Patients must have had sufficient time between a prior therapy and resolution of toxicities from the prior therapies prior to registration as follows: - Prior EGFR inhibitor: A minimum of 7 days must have elapsed from the last dose of the prior EGFR inhibitor and resolution of any drug-related toxicity to =< grade 1 except for alopecia. - Chemotherapy: A minimum of 21 days must have elapsed from the last dose and resolution of toxicity to =< grade 1 excluding =< grade 2 peripheral neuropathy or alopecia. - Surgery: A minimum of 21 days must have elapsed following major surgery and resolution of toxicity to < grade 1 and complete wound healing must have occurred. - Radiation: A minimum of 14 days must have elapsed following radiation therapy and resolution of all radiation induced toxicity excluding alopecia. - Treatment with an investigational drug: A minimum of 3 months or five half-lives of the compound, whichever is greater, must have elapsed from last treatment date.",
+  "Ability to understand and the willingness to sign a written informed consent document.",
+  "Available for follow-up of their disease after treatment until progressive disease is documented and resolution of related adverse events until < grade 2."
+],
+  "protocol_no": "123-020",
+  "short_title": "Testing the Combination of MLN0128 (TAK-228) and AZD9291 in Advanced EGFR (Epidermal Growth Factor Receptor) Mutation Positive Non-small Cell Lung Cancer",
+  "site_list": {
+  "site": [
+    {
+      "coordinating_center": "Y",
+      "site_name": "Princess Margaret Cancer Centre",
+      "site_status": "Closed to Accrual",
+      "uses_cancer_center_irb": "Y"
+    },
+    {
+      "coordinating_center": "Y",
+      "site_name": "BCCA-Vancouver Cancer Centre",
+      "site_status": "Closed to Accrual",
+      "uses_cancer_center_irb": "Y"
+    }
+  ]
+},
+  "sponsor_list": {
+  "sponsor": [
+    {
+      "is_principal_sponsor": "Y",
+      "sponsor_name": "National Cancer Institute"
+    }
+  ]
+},
+  "staff_list": {
+  "protocol_staff": [
+    {
+      "email_address": "Penelope.Bradbury@uhn.ca",
+      "first_name": "Penelope",
+      "institution_name": "Princess Margaret Cancer Centre",
+      "last_name": "Bradbury",
+      "staff_role": "Overall Principal Investigator"
+    }
+  ]
+},
+  "status": "Open to Accrual",
+  "treatment_list": {
+  "step": [
+    {
+      "arm": [
+        {
+          "arm_code": "Treatment (sapanisertib, osimertinib)",
+          "arm_description": "Patients receive sapanisertib PO QD on days 1, 3, 5, 8, 10, 12, 15, 17, 19, 22, 24, and 26 (day 1 is omitted in cycle 1). Patients also receive osimertinib PO QD on days 1-28. Cycles repeat every 28 days in the absence of disease progression or unacceptable toxicity.",
+          "arm_internal_id": 1,
+          "arm_suspended": "N",
+          "dose_level": [
+            {
+              "level_code": "Osimertinib",
+              "level_description": "Osimertinib (AZD9291) will be delivered once per day in a continuous oral dose in a 28 day cycle. The initial dose of osimertinib (AZD9291) will start at the RP2D of osimertinib (AZD9291).",
+              "level_internal_id": 1,
+              "level_suspended": "N"
+            },
+            {
+              "level_code": "Sapanisertib",
+              "level_description": "MLN0128 (TAK-228) will be delivered orally three times per week. The starting dose of MLN0128 (TAK-228) will be 2mg.",
+              "level_internal_id": 2,
+              "level_suspended": "N"
+            }
+          ],
+          "match": [
+            {
+              "and": [
+                {
+                  "clinical": {
+                    "age_numerical": ">=18",
+                    "oncotree_primary_diagnosis": "Non-Small Cell Lung Cancer"
+                  }
+                },
+                {
+                  "or": [
+                    {
+                      "genomic": {
+                        "hugo_symbol": "EGFR",
+                        "protein_change": "p.L858R",
+                        "variant_category": "Mutation",
+                        "variant_classification": "Missense_Mutation"
+                      }
+                    },
+                    {
+                      "genomic": {
+                        "hugo_symbol": "EGFR",
+                        "protein_change": "p.729_761del",
+                        "variant_category": "Mutation"
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+}
+
