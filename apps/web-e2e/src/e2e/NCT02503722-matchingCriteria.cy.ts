b739a89a91697ebdf2dfd1e05d921067996e4a54
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-03-24 13:09:24 -0400

    CTM103 bug fix, string to number changed

--- apps/web-e2e/src/e2e/NCT02503722-matchingCriteria.cy.ts
+++ apps/web-e2e/src/e2e/NCT02503722-matchingCriteria.cy.ts
@@ -66,62 +66,61 @@ import * as yaml from 'js-yaml';
 
 describe('CTIMS Trial Editor', () => {
   before(() => cy.visit('/'));
-  //deleteDownloadsFolderBeforeAll()
-   it('should Validate the Trial Editor Page', () => {
-     cy.title().should('contain', 'CTIMS')
-     trialEditorLeftPanelList().should('have.length', '8')
-     cy.trialInformation(NCT02503722_Osimertinib.nct_id,
-       "My Trial",
-       "John Doe",
-       "Draft",
-       NCT02503722_Osimertinib.long_title,
-       NCT02503722_Osimertinib.short_title,
-       NCT02503722_Osimertinib.phase,
-       NCT02503722_Osimertinib.protocol_no,
-       NCT02503722_Osimertinib.nct_purpose,
-       NCT02503722_Osimertinib.status)
+  deleteDownloadsFolderBeforeAll()
+  it('should Validate the Trial Editor Page', () => {
+    cy.title().should('contain', 'CTIMS')
+    trialEditorLeftPanelList().should('have.length', '8')
+    cy.trialInformation(NCT02503722_Osimertinib.nct_id,
+      "My Trial",
+      "John Doe",
+      "Draft",
+      NCT02503722_Osimertinib.long_title,
+      NCT02503722_Osimertinib.short_title,
+      NCT02503722_Osimertinib.phase,
+      NCT02503722_Osimertinib.protocol_no,
+      NCT02503722_Osimertinib.nct_purpose,
+      NCT02503722_Osimertinib.status)
+    // Prior treatment requirements
+    cy.priorTreatmentRequirement(NCT02503722_Osimertinib.prior_treatment_requirements[0])
+    //Age
+    cy.age(NCT02503722_Osimertinib.age)
 
-     //Age
-     cy.age(NCT02503722_Osimertinib.age)
+    //Drug List
+    cy.drugList(NCT02503722_Osimertinib.drug_list.drug[0].drug_name)
 
-     //Drug List
-     cy.drugList(NCT02503722_Osimertinib.drug_list.drug[0].drug_name)
+    //Management Group List has 1-text field and 1-Checkbox
+    cy.managementGroupList(NCT02503722_Osimertinib.management_group_list.management_group[0].management_group_name,
+      NCT02503722_Osimertinib.management_group_list.management_group[0].is_primary)
 
-     //Management Group List has 1-text field and 1-Checkbox
-     cy.managementGroupList(NCT02503722_Osimertinib.management_group_list.management_group[0].management_group_name,
-       NCT02503722_Osimertinib.management_group_list.management_group[0].is_primary)
+    //Site List has 2-Text field and 2-checkbox
+    cy.siteList(NCT02503722_Osimertinib.site_list.site[0].site_name,
+      NCT02503722_Osimertinib.site_list.site[0].site_status,
+      NCT02503722_Osimertinib.site_list.site[0].coordinating_center,
+      NCT02503722_Osimertinib.site_list.site[0].uses_cancer_center_irb)
 
-     //Site List has 2-Text field and 2-checkbox
-     cy.siteList(NCT02503722_Osimertinib.site_list.site[0].site_name,
-       NCT02503722_Osimertinib.site_list.site[0].site_status,
-       NCT02503722_Osimertinib.site_list.site[0].coordinating_center,
-       NCT02503722_Osimertinib.site_list.site[0].uses_cancer_center_irb)
+    //Sponsor List
+    cy.sponsorList(NCT02503722_Osimertinib.sponsor_list.sponsor[0].sponsor_name,
+      NCT02503722_Osimertinib.sponsor_list.sponsor[0].is_principal_sponsor)
+    //Staff List
+    cy.staffList(NCT02503722_Osimertinib.staff_list.protocol_staff[0].first_name,
+      NCT02503722_Osimertinib.staff_list.protocol_staff[0].last_name,
+      NCT02503722_Osimertinib.staff_list.protocol_staff[0].email_address,
+      NCT02503722_Osimertinib.staff_list.protocol_staff[0].institution_name,
+      NCT02503722_Osimertinib.staff_list.protocol_staff[0].staff_role)
 
-     //Sponsor List
-     cy.sponsorList(NCT02503722_Osimertinib.sponsor_list.sponsor[0].sponsor_name,
-       NCT02503722_Osimertinib.sponsor_list.sponsor[0].is_principal_sponsor)
-     //Staff List
-     cy.staffList(NCT02503722_Osimertinib.staff_list.protocol_staff[0].first_name,
-       NCT02503722_Osimertinib.staff_list.protocol_staff[0].last_name,
-       NCT02503722_Osimertinib.staff_list.protocol_staff[0].email_address,
-       NCT02503722_Osimertinib.staff_list.protocol_staff[0].institution_name,
-       NCT02503722_Osimertinib.staff_list.protocol_staff[0].staff_role)
+    //Arm code
+    cy.arm(NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_code,
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_description,
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_internal_id,
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_suspended)
 
-     //Arm code
-     cy.arm(NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_code,
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_description,
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_internal_id,
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].arm_suspended)
-
-     //Level code
-     cy.doseLevel(NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_code,
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_description,
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_internal_id.toString(),
-       NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_suspended)
-     //Click on Plus Icon to add another Dose Level
-
-   })
-
+    //Level code
+    cy.doseLevel(NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_code,
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_description,
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_internal_id.toString(),
+      NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0].level_suspended)
+    //Click on Plus Icon to add another Dose Level
+  })
    it('should validate the Matching criteria modal', () => {
     //click Match criteria
     getEditMatchingCriteria().click()
@@ -207,19 +206,19 @@ describe('CTIMS Trial Editor', () => {
     })
   })
 
-  it.skip('should click on the Export button and then Export as "JSON" file ',  () => {
+  it('should click on the Export button and then Export as "JSON" file ',  () => {
     trialEditorHeaderButtons().eq(1).should('contain','Export').click()
     trialEditorRadioButtons().eq(0).should('contain.html','json')
     cy.get('[type="radio"]').first().check({force: true}).should('be.checked')
     trialEditorExportCtml().eq(1).should('contain','Export CTML').click()
   });
 
-  it.skip('should click on the Export button and then Export as "YAML" file ',  () => {
+  it('should click on the Export button and then Export as "YAML" file ',  () => {
     trialEditorRadioButtons().eq(1).click({force: true})
     trialEditorExportCtml().eq(1).should('contain','Export CTML').click()
   });
 
-  it.skip('should compare json and Yaml ',  () => {
+  it('should compare json and Yaml ',  () => {
     // Load the JSON file
       cy.readFile('/Users/srimathijayasimman/WebstormProjects/CTIMS/ctims/apps/web-e2e/cypress/downloads/ctml-model.json', 'utf-8').then((jsonData) => {
         const json = jsonData
@@ -313,15 +312,15 @@ describe('CTIMS Trial Editor', () => {
       it('should validate the match of the "Treatment list Arm Level" values',() => {
         let jsonVal = NCT02503722_Osimertinib.treatment_list.step[0].arm[0]
         //let testDataMatchingCriteria
-        let testDataMatchingCriteria: string[] = [jsonVal.arm_code,
+        let testDataMatchingCriteria = [jsonVal.arm_code,
           jsonVal.arm_description,
-          jsonVal.arm_internal_id.toString(),
+          jsonVal.arm_internal_id,
           jsonVal.arm_suspended,
         ]
         cy.log(JSON.stringify(testDataMatchingCriteria))
         cy.readFile('/Users/srimathijayasimman/WebstormProjects/CTIMS/ctims/apps/web-e2e/cypress/downloads/ctml-model.json', 'utf-8').then((exportedCtmlModel) => {
           let exportData = exportedCtmlModel.treatment_list.step[0].arm[0]
-          let ctmlMatchingCriteria: string[] = [exportData.arm_code,
+          let ctmlMatchingCriteria = [exportData.arm_code,
             exportData.arm_description,
             exportData.arm_internal_id,
             exportData.arm_suspended,
@@ -335,16 +334,16 @@ describe('CTIMS Trial Editor', () => {
     it('should validate the match of the "Treatment list Dose Level" values',() => {
       let jsonVal = NCT02503722_Osimertinib.treatment_list.step[0].arm[0].dose_level[0]
       //let testDataMatchingCriteria
-      let testDataMatchingCriteria: string[] = [
+      let testDataMatchingCriteria = [
         jsonVal.level_code,
         jsonVal.level_description,
-        jsonVal.level_internal_id.toString(),
+        jsonVal.level_internal_id,
         jsonVal.level_suspended
       ]
       cy.log(JSON.stringify(testDataMatchingCriteria))
       cy.readFile('/Users/srimathijayasimman/WebstormProjects/CTIMS/ctims/apps/web-e2e/cypress/downloads/ctml-model.json', 'utf-8').then((exportedCtmlModel) => {
         let exportData = exportedCtmlModel.treatment_list.step[0].arm[0].dose_level[0]
-        let ctmlMatchingCriteria: string[] = [
+        let ctmlMatchingCriteria = [
           exportData.level_code,
           exportData.level_description,
           exportData.level_internal_id,
