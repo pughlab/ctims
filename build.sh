769497e699df66c7cc44690556fd8bf9cfc91b4e
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-04-17 17:39:13 -0400

    docker vars and push

--- build.sh
+++ build.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
-
-SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-echo "SELFDIR: $SELFDIR"
-CTIMS_WEB_CONTAINER_IMAGE_NAME=ctims-web
-CTIMS_WEB_CONTAINER_IMAGE_LOCATION=$TECHNA_REGISTRY_ENDPOINT:$TECHNA_REGISTRY_PORT/$CTIMS_WEB_CONTAINER_IMAGE_NAME
-
-GIT_REF="$(git rev-parse @)"
-GIT_BRANCH="$(git symbolic-ref --short HEAD)"
-GIT_BRANCH_SAFE="$(echo "$GIT_BRANCH" | tr -dc '[:alnum:]' | tr '[:upper:]' '[:lower:]')"
-GIT_IS_CLEAN="$(git diff-index --quiet HEAD && echo yes || echo no)"
-COMMIT_ISH="$(git describe --tags --all --always | grep --color=never -o -E '[^\/]+$' | sed "s/~/-/g")"
-PROJECT=ctims
-CONTAINER_NAME=ctims-web
-
+source ./vars.sh
 echo "CTIMS_WEB_CONTAINER_IMAGE_LOCATION: $CTIMS_WEB_CONTAINER_IMAGE_LOCATION"
 echo "GIT_REF: $GIT_REF"
 echo "GIT_BRANCH: $GIT_BRANCH"
