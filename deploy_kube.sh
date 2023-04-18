68e5bf6657cbabc59d065defd07a1df8bd9ac447
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-04-18 10:27:19 -0400

    fix deployment.yaml

--- deploy_kube.sh
+++ deploy_kube.sh
@@ -5,6 +5,7 @@ FQIN="ctims-${GIT_BRANCH_SAFE}"
 
 eval "helm upgrade --debug \
 --set docker.registry.endpoint=$TECHNA_REGISTRY_ENDPOINT:$TECHNA_REGISTRY_PORT \
+--set docker.image=$CTIMS_WEB_CONTAINER_IMAGE_LOCATION \
 --set git.branch=$GIT_COMMIT_ISH \
 --set git.ref=$GIT_REF \
 --set git.is_clean=$GIT_IS_CLEAN \
