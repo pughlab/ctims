7737b68b31cdca31dc11bf8eb08f902826214a4a
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-04-19 14:00:34 -0400

    add ctml json creation dto

--- apps/api/src/app/ctml-json/dto/create-ctml-json.dto.ts
+++ apps/api/src/app/ctml-json/dto/create-ctml-json.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCtmlJsonDto {}
+import {ApiProperty} from "@nestjs/swagger";
+
+export class CreateCtmlJsonDto {
+  @ApiProperty()
+  schemaVersionId: number;
+
+  @ApiProperty()
+  data: string;
+
+  @ApiProperty()
+  trialId: number
+}
