ef11b7c4bcd920a68dbcd09f0a744109b4ce3718
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-04-19 13:50:19 -0400

    add trial creation dto

--- apps/api/src/app/trial/dto/create-trial.dto.ts
+++ apps/api/src/app/trial/dto/create-trial.dto.ts
@@ -1,1 +1,35 @@
-export class CreateTrialDto {}
+import {ApiProperty, ApiPropertyOptional} from "@nestjs/swagger";
+import {status} from "@prisma/client";
+
+export class CreateTrialDto {
+  @ApiProperty()
+  nctId: string;
+
+  @ApiPropertyOptional({
+    minLength: 1,
+    maxLength: 191
+  })
+  nickname?: string;
+
+  @ApiPropertyOptional({
+    minLength: 1,
+    maxLength: 191
+  })
+  principalInvestigator?: string;
+
+  @ApiPropertyOptional({
+    enum: status
+  })
+  status: status;
+
+  @ApiProperty({
+    description: "The ID of the schema for this trial data."
+  })
+  ctml_schema_id: number;
+
+  @ApiProperty({
+    description: "JSON string for this trial."
+  })
+  ctml_json_string: string;
+
+}
