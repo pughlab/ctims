2b8470804ac250ec26b45a54cd76ef2f84d4d373
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-04-24 15:15:48 -0400

    remove old class typing

--- apps/api/src/app/trial/dto/update-trial.dto.ts
+++ apps/api/src/app/trial/dto/update-trial.dto.ts
@@ -1,9 +1,7 @@
 
 import { CreateTrialDto } from './create-trial.dto';
-import {OmitType, PartialType} from "@nestjs/swagger";
+import {PartialType} from "@nestjs/swagger";
 
 // Omit the JSON string input, as updating should be specific to just the trial.
 // To update the JSON see the ctml_json controller
-export class UpdateTrialDto extends PartialType(
-  OmitType(CreateTrialDto, ['ctml_json_string'])
-) {}
+export class UpdateTrialDto extends PartialType(CreateTrialDto) {}
