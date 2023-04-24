d34a715943e0bc7886e7bf4a3783c6bcd5312591
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-04-24 15:43:29 -0400

    add documentation to schema endpoints

--- apps/api/src/app/schema-ctml/ctml-schema.controller.ts
+++ apps/api/src/app/schema-ctml/ctml-schema.controller.ts
@@ -2,7 +2,14 @@ import {Controller, Get, Post, Body, Patch, Param, Delete, NotImplementedExcepti
 import { CtmlSchemaService } from './ctml-schema.service';
 import { CreateCtmlSchemaDto } from './dto/create-ctml-schema.dto';
 import { UpdateCtmlSchemaDto } from './dto/update-ctml-schema.dto';
-import {ApiResponse, ApiTags} from "@nestjs/swagger";
+import {
+  ApiCreatedResponse,
+  ApiFoundResponse,
+  ApiNoContentResponse, ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags
+} from "@nestjs/swagger";
 
 @Controller('ctml-schemas')
 @ApiTags("Schema CTML")
@@ -10,30 +17,38 @@ export class CtmlSchemaController {
   constructor(private readonly schemaCtmlService: CtmlSchemaService) {}
 
   @Post()
-  @ApiResponse({ status: HttpStatus.CREATED, description: "New CTML schema created." })
+  @ApiOperation({ summary: "Create a CTML schema record" })
+  @ApiCreatedResponse({ description: "New CTML schema created." })
   create(@Body() createSchemaCtmlDto: CreateCtmlSchemaDto) {
     return this.schemaCtmlService.create(createSchemaCtmlDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Get all CTML schema records" })
+  @ApiFoundResponse({ description: "CTML schema records found." })
   findAll() {
-    throw new NotImplementedException();
     return this.schemaCtmlService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: "Find a CTML schema" })
+  @ApiFoundResponse({ description: "CTML schema found." })
   findOne(@Param('id') id: string) {
     throw new NotImplementedException();
     return this.schemaCtmlService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: "Update a CTML schema" })
+  @ApiOkResponse({ description: "CTML schema updated." })
   update(@Param('id') id: string, @Body() updateSchemaCtmlDto: UpdateCtmlSchemaDto) {
     throw new NotImplementedException();
     return this.schemaCtmlService.update(+id, updateSchemaCtmlDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: "Delete a CTML schema record" })
+  @ApiNoContentResponse({ description: "CTML schema deleted." })
   remove(@Param('id') id: string) {
     throw new NotImplementedException();
     return this.schemaCtmlService.remove(+id);
