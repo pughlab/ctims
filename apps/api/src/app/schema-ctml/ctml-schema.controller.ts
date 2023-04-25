bf108140be3d7563c51d874fad0a1164bf7e820f
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-04-25 14:16:33 -0400

    add login guards

--- apps/api/src/app/schema-ctml/ctml-schema.controller.ts
+++ apps/api/src/app/schema-ctml/ctml-schema.controller.ts
@@ -1,14 +1,27 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, NotImplementedException, HttpStatus} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotImplementedException,
+  HttpStatus,
+  UseGuards
+} from '@nestjs/common';
 import { CtmlSchemaService } from './ctml-schema.service';
 import { CreateCtmlSchemaDto } from './dto/create-ctml-schema.dto';
 import { UpdateCtmlSchemaDto } from './dto/update-ctml-schema.dto';
 import {
+  ApiBearerAuth,
   ApiCreatedResponse,
   ApiFoundResponse,
   ApiNoContentResponse, ApiOkResponse,
   ApiOperation,
   ApiTags
 } from "@nestjs/swagger";
+import { KeycloakPasswordGuard } from "../auth/KeycloakPasswordGuard";
 
 @Controller('ctml-schemas')
 @ApiTags("Schema CTML")
@@ -16,6 +29,8 @@ export class CtmlSchemaController {
   constructor(private readonly schemaCtmlService: CtmlSchemaService) {}
 
   @Post()
+  @UseGuards(KeycloakPasswordGuard)
+  @ApiBearerAuth("KeycloakPasswordGuard")
   @ApiOperation({ summary: "Create a CTML schema record" })
   @ApiCreatedResponse({ description: "New CTML schema created." })
   create(@Body() createSchemaCtmlDto: CreateCtmlSchemaDto) {
@@ -23,6 +38,8 @@ export class CtmlSchemaController {
   }
 
   @Get()
+  @UseGuards(KeycloakPasswordGuard)
+  @ApiBearerAuth("KeycloakPasswordGuard")
   @ApiOperation({ summary: "Get all CTML schema records" })
   @ApiFoundResponse({ description: "CTML schema records found." })
   findAll() {
@@ -30,6 +47,8 @@ export class CtmlSchemaController {
   }
 
   @Get(':id')
+  @UseGuards(KeycloakPasswordGuard)
+  @ApiBearerAuth("KeycloakPasswordGuard")
   @ApiOperation({ summary: "Find a CTML schema" })
   @ApiFoundResponse({ description: "CTML schema found." })
   findOne(@Param('id') id: string) {
@@ -44,6 +63,8 @@ export class CtmlSchemaController {
   }
 
   @Delete(':id')
+  @UseGuards(KeycloakPasswordGuard)
+  @ApiBearerAuth("KeycloakPasswordGuard")
   @ApiOperation({ summary: "Delete a CTML schema record" })
   @ApiNoContentResponse({ description: "CTML schema deleted." })
   async remove(@Param('id') id: string) {
