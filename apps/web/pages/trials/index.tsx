e7637ff6ed7ae7c172c4172c6c78a6d82c92d610
Mickey Ng <mickey.ng@rmp.uhn.ca>
2023-04-28 14:09:19 -0400

    CTM-28 sort by CTML status

--- apps/web/pages/trials/index.tsx
+++ apps/web/pages/trials/index.tsx
@@ -54,17 +54,25 @@ const Trials = () => {
         id: 1,
         nickname: 'Trial 1',
         principalInvestigator: 'Dr. John Doe',
-        status: 'Active',
+        status: 'Draft',
         createdOn: '2021-01-01 by Dr. John Doe',
         updatedOn: '2021-01-01 by Dr. John Doe',
       },
       {
         id: 2,
         nickname: 'Trial 2',
-        principalInvestigator: 'Dr. John Doe',
-        status: 'Active',
-        createdOn: '2021-01-01 by Dr. John Doe',
-        updatedOn: '2021-01-01 by Dr. John Doe',
+        principalInvestigator: 'Dr. Who',
+        status: 'In review',
+        createdOn: '2021-02-02 by Dr. Who',
+        updatedOn: '2021-02-02 by Dr. Who',
+      },
+      {
+        id: 3,
+        nickname: 'Trial 3',
+        principalInvestigator: 'Dr. Dolittle',
+        status: 'Complete',
+        createdOn: '2021-03-03 by Dr. Dolittle',
+        updatedOn: '2021-03-03 by Dr. Dolittle',
       }
     ]
     setTrials(trials);
@@ -124,13 +132,14 @@ const Trials = () => {
           <DataTable value={trials} rowHover={true}
                      onRowMouseEnter={(event) => setRowEntered(event.data) }
                      onRowMouseLeave={() => setRowEntered(null) }
+                     sortField="createdOn" sortOrder={-1}
           >
             <Column field="id" header="ID" ></Column>
             <Column field="id" header="" body={subMenuTemplate}></Column>
             <Column field="nickname" header="Nickname"></Column>
             <Column field="principalInvestigator" header="Principal Investigator" ></Column>
-            <Column field="status" header="Status"></Column>
-            <Column field="createdOn" header="Created on"></Column>
+            <Column field="status" header="Status" sortable></Column>
+            <Column field="createdOn" header="Created on" ></Column>
             <Column field="updatedOn" header="Modified on"></Column>
           </DataTable>
         </div>
