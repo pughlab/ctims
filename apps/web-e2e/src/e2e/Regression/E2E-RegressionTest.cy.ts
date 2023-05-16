714d795c092f41d05a16c018845ea30d470970a2
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-05-16 17:08:10 -0400

    CTM-192-save & delete

--- apps/web-e2e/src/e2e/Regression/E2E-RegressionTest.cy.ts
+++ apps/web-e2e/src/e2e/Regression/E2E-RegressionTest.cy.ts
@@ -1,2 +1,4 @@
-import "../CtmTest/CTM-105-NCT02503722_Osimertinib.cy"
-import "../CtmTest/CTM-114-NCT03297606_CAPTUR.cy"
+//import "../CtmTest/CTM-105-NCT02503722_Osimertinib.cy"
+//import "../CtmTest/CTM-114-NCT03297606_CAPTUR.cy"
+import  "../CtmTest/CTM-192-NCT02503722_Osimertinib-delete.cy"
+import "../CtmTest/copy.cy"
