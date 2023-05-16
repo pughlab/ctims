bcc6cb67bcc80f01776d9214d1211c1bccfe4c88
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-05-16 17:08:10 -0400

    CTM-192-save & delete

--- apps/web-e2e/src/e2e/Base/dateClass.cy.ts
+++ apps/web-e2e/src/e2e/Base/dateClass.cy.ts
@@ -7,18 +7,6 @@ class dateClass {
     const day = ("0" + date.getDate()).slice(-2); // add leading zero if day is less than 10
     const formattedDate = year + '-' + month + '-' + day;
     return formattedDate
-    /*date = new Date();
-    //const options = {month: 'short', day: 'numeric', year: 'numeric'} //didn't use time, since test fails in 1 sec
-    // difference when uploaded
-    const options = {month: 'short', day: 'numeric', year: 'numeric'} //didn't use time, since test fails in 1 sec difference when uploaded
-
-    //{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short' };
-    // @ts-ignore
-    //RDS_May 4,2023.zip
-    //NCT02503722_2023-05-12.json
-    const formattedDate = new Intl.DateTimeFormat('en-CA', options).format(date);
-    cy.log(formattedDate)
-    return formattedDate*/
   }
 }
 export default new dateClass()
