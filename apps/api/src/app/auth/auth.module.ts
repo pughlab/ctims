65fe8ce50e3960c9dc7ce0f3e7aaa7ca4650bf78
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-05-25 15:24:05 -0400

    record login events at failure and success points

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { KeycloakPasswordStrategy } from "./keycloak-password.strategy";
 import {AuthController} from "./auth.controller";
+import { EventService } from "../event/event.service";
 
 @Module({
   imports: [PassportModule],
-  providers: [KeycloakPasswordStrategy],
+  providers: [KeycloakPasswordStrategy, EventService],
   exports: [PassportModule, KeycloakPasswordStrategy],
   controllers: [AuthController]
 })
