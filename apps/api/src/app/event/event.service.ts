950fbdcdd1e6e9d308cdf25765862a301d7a179b
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-05-25 15:23:44 -0400

    add event creation service func and interface

--- apps/api/src/app/event/event.service.ts
+++ apps/api/src/app/event/event.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { event } from "@prisma/client";
+import { ICreateEvent } from "./create-event.interface";
+import { mockSession } from "next-auth/client/__tests__/helpers/mocks";
+import user = mockSession.user;
 
 @Injectable()
 export class EventService {
@@ -8,7 +11,12 @@ export class EventService {
   constructor(
     private readonly prismaService: PrismaService
   ) { }
-  async createEvent(data: event): Promise<event> {
-    return this.prismaService.event.create({ data });
+  async createEvent(data: ICreateEvent): Promise<event> {
+    return this.prismaService.event.create({
+      data: {
+        ...data,
+        user: { connect: { id: data.user.id }},
+      }
+    });
   }
 }
