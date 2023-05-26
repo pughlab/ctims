5155121032a354935dc18e65c397a17233c482ed
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-05-26 14:57:10 -0400

    conditionally add connections to event creation

--- apps/api/src/app/event/event.service.ts
+++ apps/api/src/app/event/event.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { event } from "@prisma/client";
 import { ICreateEvent } from "./create-event.interface";
-import { mockSession } from "next-auth/client/__tests__/helpers/mocks";
-import user = mockSession.user;
 
 @Injectable()
 export class EventService {
@@ -15,7 +13,10 @@ export class EventService {
     return this.prismaService.event.create({
       data: {
         ...data,
-        user: { connect: { id: data.user.id }},
+        user: data.user ? { connect: { id: data.user.id }} : undefined,
+        trial: data.trial ? { connect: { id: data.trial.id }} : undefined,
+        ctml_json: data.ctml_json ? { connect: { id: data.ctml_json.id }} : undefined,
+        ctml_schema: data.ctml_schema ? { connect: { id: data.ctml_schema.id }} : undefined
       }
     });
   }
