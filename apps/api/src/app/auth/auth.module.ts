3eb96b14a441064b8faaa262ca218644e9bf447f
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-05-26 16:12:22 -0400

    remove EventService from list of providers

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { KeycloakPasswordStrategy } from "./keycloak-password.strategy";
 import {AuthController} from "./auth.controller";
-import { EventService } from "../event/event.service";
 
 @Module({
   imports: [PassportModule],
-  providers: [KeycloakPasswordStrategy, EventService],
+  providers: [KeycloakPasswordStrategy],
   exports: [PassportModule, KeycloakPasswordStrategy],
   controllers: [AuthController]
 })
--- apps/api/src/app/trial/trial.module.ts
+++ apps/api/src/app/trial/trial.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TrialService } from './trial.service';
 import { TrialController } from './trial.controller';
 import {PrismaService} from "../prisma.service";
-import { EventService } from "../event/event.service";
 
 @Module({
   controllers: [TrialController],
-  providers: [TrialService, PrismaService, EventService]
+  providers: [TrialService, PrismaService]
 })
 export class TrialModule {}
