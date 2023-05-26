2478101be7aecf85bcd19883e172cf7945990866
Pietro Andreoli <pietro.andreoli@uhn.ca>
2023-05-26 14:56:39 -0400

    add references to other entities to interface

--- apps/api/src/app/event/create-event.interface.ts
+++ apps/api/src/app/event/create-event.interface.ts
@@ -1,8 +1,13 @@
-import { event_type, Prisma, user } from "@prisma/client";
+import { event_type, Prisma, user, trial, ctml_json, ctml_schema } from "@prisma/client";
 
 export interface ICreateEvent {
   type: event_type,
   description?: string,
   metadata?: Prisma.JsonValue,
-  user?: user
+  // The only relevant property on the below Prisma entity types is the 'id' property, so pick just that as the type
+  // This allows the user of a Prisma entity, or a simple { id: 1 } object.
+  user?: Pick<user, 'id'>,
+  trial?: Pick<trial, 'id'>,
+  ctml_json?: Pick<ctml_json, 'id'>,
+  ctml_schema?: Pick<ctml_schema, 'id'>
 }
