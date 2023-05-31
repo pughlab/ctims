92d77b2a8997e35c37e56cd07b803854156f37df
artonio <asukhovatkin@gmail.com>
2023-05-31 15:59:39 -0400

    added keycloak env vars to kubernetes

--- deploy_kube.sh
+++ deploy_kube.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ./vars.sh
+source /etc/environment
 
 FQIN="ctims-${GIT_BRANCH_SAFE}"
 
@@ -8,6 +9,10 @@ eval "helm upgrade --debug \
 --set docker.image=$CTIMS_WEB_CONTAINER_IMAGE_LOCATION \
 --set docker.dbImage=$CTIMS_DB_CONTAINER_IMAGE_LOCATION \
 --set docker.apiImage=$CTIMS_API_CONTAINER_IMAGE_LOCATION \
+--set keycloak.url=$KEYCLOAK_URL \
+--set keycloak.realm=$KEYCLOAK_REALM \
+--set keycloak.clientId=$KEYCLOAK_CLIENT_ID \
+--set keycloak.clientSecret=$KEYCLOAK_CLIENT_SECRET \
 --set git.branch=$GIT_COMMIT_ISH \
 --set git.ref=$GIT_REF \
 --set git.is_clean=$GIT_IS_CLEAN \
