06f4e7c8927c46586b55319e935effb269a277b2
Mickey Ng <mickey.ng@rmp.uhn.ca>
2023-06-08 15:43:37 -0400

    add scripts to create and seed DB for local environment

--- apps/api/prisma/seed/index.js
+++ apps/api/prisma/seed/index.js
@@ -1,14 +1,26 @@
 const { PrismaClient } = require('@prisma/client')
+const fs = require('fs');
+
 
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      email: 'anton@gmail.com',
-      name: 'anton',
-    }
-  })
+  try {
+    const ctml_json = fs.readFileSync('database/ctml_schema.json', 'utf8');
+
+    const ctmlSchema = await prisma.ctml_schema.create({
+      data: {
+        version: 1,
+        schema: ctml_json,
+      },
+    });
+
+    console.log('Database seeded successfully.');
+  } catch (error) {
+    console.error('Error seeding database:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 main().then(() => {
