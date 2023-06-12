ca258482983db183c8446844c50af918afc5e8c6
srimathijayasimman <Srimathi.Jayasimman@uhn.ca>
2023-06-12 11:19:08 -0400

    CTM-user roles

--- apps/web-e2e/cypress.config.ts
+++ apps/web-e2e/cypress.config.ts
@@ -29,7 +29,8 @@ const cypressJsonConfig: EndToEndConfigOptions = {
  // specPattern: 'src/e2e/CtmTest/CTM-114-NCT03297606_CAPTUR.cy.ts',
   //specPattern: 'src/e2e/CtmTest/CTM-194-Save-Edit-Delete/CTM-194-NCT03297606_CAPTUR-Edit.cy.ts',
   //specPattern: 'src/e2e/Regression/E2E-RegressionTest.cy.ts',
-  specPattern: 'src/e2e/CtmTest/CTM-204/CTM-204-NCT03114319_TNO155.cy.ts',
+  //specPattern: 'src/e2e/CtmTest/CTM-204/CTM-204-NCT03114319_TNO155.cy.ts',
+  specPattern: 'src/e2e/CtmTest/User-Roles/test-member.cy.ts',
 
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 30000,
@@ -46,8 +47,8 @@ module.exports = defineConfig({
   //viewportHeight: 1080,
   "env": {
     //"baseUrl": "http://localhost:4200/",
-    //baseUrl: 'http://localhost:3000',
-    baseUrl: 'https://ctims-web.qa02.technainstitute.net',
+      baseUrl: 'http://localhost:3000',
+    //baseUrl: 'https://ctims-web.qa02.technainstitute.net',
     defaultCommandTimeout: 30000,
     allureReuseAfterSpec: true,
     allureResultsPath: "allure-results",
