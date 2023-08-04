bfeae5a7a20e3f49bb7f92b422da75e4fe77b1d6
Mickey Ng <mickey.ng@rmp.uhn.ca>
2023-08-04 13:23:44 -0400

    CTM-256 add warning to save before export

--- apps/web/components/editor/ExportCtmlDialog.tsx
+++ apps/web/components/editor/ExportCtmlDialog.tsx
@@ -27,14 +27,11 @@ const ExportCtmlDialog = (props: ExportCtmlDialogProps) => {
   const ctmlModel = useSelector((state: RootState) => state.finalModelAndErrors.ctmlModel);
   const trialId = useSelector((state: RootState) => state.context.trialId);
   const isGroupAdmin = useSelector((state: RootState) => state.context.isTrialGroupAdmin);
-
+  const isFormChanged = useSelector((state: RootState) => state.context.isFormChanged);
 
   useEffect(() => {
-    if(ctmlModel === null) {
-    console.log('ctmlModel', ctmlModel);
-      setExportButtonDisabled(true);
-    }
-  }, [ctmlModel])
+    setExportButtonDisabled(shouldDisableExportButton());
+  }, [ctmlModel, isFormChanged, errors])
 
   useEffect(() => {
     setIsDialogVisible(props.isDialogVisible);
@@ -55,21 +52,13 @@ const ExportCtmlDialog = (props: ExportCtmlDialogProps) => {
 
   }, [errorSchema])
 
-  useEffect(() => {
-
-    if (errors.length > 0) {
-      setExportButtonDisabled(true);
-    }
-    if (errors.length === 0) {
-      if (ctmlModel !== null) {
-        setExportButtonDisabled(false);
-      }
-    }
-
-  }, [errors])
-
   const onDialogHide = () => {
     props.onDialogHide();
+  }
+
+  // disable export OK button if there are errors of if form is changed and not saved
+  const shouldDisableExportButton = () => {
+    return errors.length > 0 || isFormChanged || ctmlModel === null;
   }
 
   const footer = (props: {exportCtmlClicked: () => void}) => {
@@ -104,6 +93,13 @@ const ExportCtmlDialog = (props: ExportCtmlDialogProps) => {
     )
   }
 
+  const notSavedErrorContent = () => {
+    return (
+      <>
+        <div>You must save the CTML before you can export it.</div>
+      </>
+    )
+  }
 
   const doExport = () => {
 
@@ -173,6 +169,15 @@ const ExportCtmlDialog = (props: ExportCtmlDialogProps) => {
             marginBottom: '10px'
           }}
           content={errorContent}
+        />)}
+        {isFormChanged && (<Message
+          severity="error"
+          style={{
+            whiteSpace: "pre-line",
+            marginLeft: '18px',
+            marginBottom: '10px'
+          }}
+          content={notSavedErrorContent}
         />)}
       </div>
       {isGroupAdmin ? <div style={{marginLeft: '30px'}}>
