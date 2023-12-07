cfd51339548194841c6483e7113c1753de532874
Anton <anton.sukhovatkin@rmp.uhn.ca>
2023-12-07 15:06:37 -0500

    update jobs 3

--- docker_build_backend_qa.sh
+++ docker_build_backend_qa.sh
@@ -3,4 +3,4 @@ set -a
 . /home/qa_env_vars.sh
 set +a
 
-docker compose  -f docker-compose-qa.yml build --no-cache backend_qa
+docker compose -f docker-compose-qa.yml build --no-cache backend_qa
--- docker_build_frontend_qa.sh
+++ docker_build_frontend_qa.sh
@@ -3,4 +3,4 @@ set -a
 . /home/qa_env_vars.sh
 set +a
 
-docker compose build -f docker-compose-qa.yml --no-cache frontend_qa
+docker compose -f docker-compose-qa.yml build  --no-cache frontend_qa
