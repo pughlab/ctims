d0fd436e01b8adf84dc3fd30204192c72b55270f
mickey-ng <mickey.ng@uhn.ca>
2024-01-04 16:25:07 -0500

    update key file path

--- docker_build_mm_api_qa.sh
+++ docker_build_mm_api_qa.sh
@@ -3,8 +3,9 @@ set -a
 . /home/qa_env_vars.sh
 set +a
 
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 export DOCKER_BUILDKIT=1
 eval `ssh-agent`
-ssh-add $(eval echo ~$USER)/.ssh/my_key
+ssh-add $USER_HOME/.ssh/my_key
 docker compose -f docker-compose-qa.yml build --no-cache --ssh default=$SSH_AUTH_SOCK mm_api_qa
 
