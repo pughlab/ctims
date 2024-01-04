057a4a873ba6387ee331197128eaac5d2cec8636
mickey-ng <mickey.ng@uhn.ca>
2024-01-04 15:11:01 -0500

    CTM-375 fix protocol_no when it is undefined

--- docker_build_mm_api_qa.sh
+++ docker_build_mm_api_qa.sh
@@ -3,4 +3,8 @@ set -a
 . /home/qa_env_vars.sh
 set +a
 
-docker compose -f docker-compose-qa.yml build --no-cache mm_api_qa
+export DOCKER_BUILDKIT=1
+eval `ssh-agent`
+ssh-add ~/.ssh/my_key
+docker compose -f docker-compose-qa.yml build --no-cache --ssh default=$SSH_AUTH_SOCK mm_api_qa
+
