1d00329cd58f7414c8f7f6dd5c863df2f09378c1
mickey-ng <mickey.ng@uhn.ca>
2024-01-12 17:11:23 -0500

    update ssh key location depending on build system

--- docker_build_mm_api_qa.sh
+++ docker_build_mm_api_qa.sh
@@ -3,9 +3,13 @@ set -a
 . /home/qa_env_vars.sh
 set +a
 
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+if [ "$(whoami)" = "jenkins" ]; then
+    USER_HOME="/home"
+else
+    USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)/.ssh
+fi
 export DOCKER_BUILDKIT=1
 eval `ssh-agent`
-ssh-add $USER_HOME/.ssh/my_key
+ssh-add $USER_HOME/my_key
 docker compose -f docker-compose-qa.yml build --no-cache --ssh default=$SSH_AUTH_SOCK mm_api_qa
 
