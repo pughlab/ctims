dc493bf89e483b5c735034fcea9bad9904d93964
mickey-ng <mickey.ng@uhn.ca>
2024-02-12 16:17:06 -0500

    CTM-409 fix delete node and state update

--- libs/ui/src/lib/components/LeftMenuComponent.tsx
+++ libs/ui/src/lib/components/LeftMenuComponent.tsx
@@ -186,12 +186,11 @@ const LeftMenuComponent = memo((props: ILeftMenuComponentProps) => {
   // when a node is deleted we update the root nodes state
   useEffect(() => {
     if (nodeKeyToBeDeleted.nodeKey) {
-      const newRootNodes = structuredClone(rootNodes);
+      const newRootNodes = [...rootNodes];
       deleteNodeFromChildrenArrayByKey(newRootNodes[0], nodeKeyToBeDeleted.nodeKey);
       setRootNodesState(newRootNodes);
       const state = store.getState();
       updateReduxViewModelAndCtmlModel(newRootNodes, state);
-
       dispatch(deleteMatchDialogError(nodeKeyToBeDeleted.nodeKey))
       dispatch(deleteNode({nodeKey: ''}));
     }
@@ -441,8 +440,6 @@ const LeftMenuComponent = memo((props: ILeftMenuComponentProps) => {
         divRef.current.addEventListener('click', onNodeClick);
 
       }, [divRef.current])
-
-
 
       return (
         <>
