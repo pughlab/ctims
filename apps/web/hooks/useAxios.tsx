a7bd2aaa1437fe87587e3ba6e48a9688f84ebc6e
mickey-ng <mickey.ng@uhn.ca>
2024-04-04 17:07:21 -0400

    fix useAxios base url

--- apps/web/hooks/useAxios.tsx
+++ apps/web/hooks/useAxios.tsx
@@ -2,10 +2,12 @@ import {useEffect, useState} from "react";
 import axios, {AxiosRequestConfig} from "axios";
 import {signOut, useSession} from "next-auth/react";
 import { useRouter } from 'next/router';
+import getConfig from "next/config";
 
 const useAxios = () => {
 
-  axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:3333/api"
+  const { publicRuntimeConfig } = getConfig();
+  axios.defaults.baseURL = publicRuntimeConfig.REACT_APP_API_URL || "http://localhost:3333/api"
 
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
