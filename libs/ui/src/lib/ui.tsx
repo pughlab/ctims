30835c0ee90f406ce41062a64dc3d9496ca0bf1e
Dheeraj Gavini <dheerajgavini@ddplaptop80.uhn.ca>
2024-08-14 12:06:38 -0400

    Changed the code implementation for better understanding

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -164,8 +164,14 @@ export const Ui = (props: UiProps) => {
     dispatch(resetActiveArmId())
     let formDataClone = structuredClone(formRef.current.state.formData)
     console.log('formRef state', formRef.current.state)
-    const transformPriorDataFromObjectToArray = formDataClone.prior_treatment_requirements?.prior_treatment_requirement && formDataClone.prior_treatment_requirements?.prior_treatment_requirement.map((item: { prior_treatment_requirement_name: any; }) => item.prior_treatment_requirement_name);
-    formDataClone = {...formDataClone, prior_treatment_requirements : transformPriorDataFromObjectToArray}
+    if(formDataClone.prior_treatment_requirements)
+    {
+      if(formDataClone.prior_treatment_requirements.prior_treatment_requirement)
+      {
+        const transformPriorDataFromObjectToArray =formDataClone.prior_treatment_requirements.prior_treatment_requirement.map((item: { prior_treatment_requirement_name: any; }) => item.prior_treatment_requirement_name);
+        formDataClone = {...formDataClone, prior_treatment_requirements : transformPriorDataFromObjectToArray}
+      }
+    }
     dispatch(setCtmlModel(formDataClone))
 
     const currentState = store.getState();
