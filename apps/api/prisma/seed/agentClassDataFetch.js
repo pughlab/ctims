976757bf4a31c14a7ec6e98324d338c5a601ba73
Dheeraj Gavini <98056650+dgavini@users.noreply.github.com>
2025-06-02 21:22:52 +0000

    Added scripts to pull Agent and Agent Class data from csv files into DB (#249)
    * Added scripts to pull Agent and Agent Class data from csv files into DB
    
    * Fixed the field mapping from csv files
    
    * Fixed the nciagent,csv file
    
    * Elaborated the error messages and made sure they are consistent
    
    * Elaborated the error messages and made sure they are consistent
    
    * Elaborated the error messages and made sure they are consistent
    
    ---------
    
    Co-authored-by: Dheeraj Gavini <dheerajgavini@Dheerajs-MacBook-Pro.local>
    Co-authored-by: Dheeraj Gavini <dheerajgavini@dheerajs-mbp.home>

---
+++ apps/api/prisma/seed/agentClassDataFetch.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const csvParser = require("csv-parser");
+const { PrismaClient } = require('@prisma/client');
+const readline = require('readline');
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+          url: `${process.env.DATABASE_URL}?connection_limit=100&pool_timeout=90`,
+        },
+      }
+    }
+  )
+async function saveAgentClass(data){
+    try{
+        await prisma.agentclass.deleteMany();
+        await prisma.agentclass.createMany({
+            data: data,
+            skipDuplicates: true,
+          });
+    }
+    catch(error){
+      console.error("Error: ", error)
+      throw error;
+    }
+    finally {
+        await prisma.$disconnect();
+      }
+}
+
+async function askForConfirmation() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  
+    return new Promise((resolve) => {
+      rl.question(
+        "This operation will delete all current data from the 'agent class' table. Do you want to continue? (y/n): ",
+        (answer) => {
+          rl.close();
+          resolve(answer.toLowerCase() === 'y');
+        }
+      );
+    });
+  }
+
+async function main(){
+    return  new Promise((resolve, reject)=>{
+        try{
+            const result = [];
+            fs.createReadStream("apps/api/prisma/metadata/nciagent/nciagentclass.csv")
+                .pipe(csvParser())
+                .on("data", (data) => {
+                    result.push(data);
+                })
+                .on("end", () => {
+                    const newResult=result.map((data)=>{
+                        var id = Number(data.ID);
+                        var agentclass = data.AgentClass.replace(/\(.*?\)/g, '');
+                        return {id, agentclass}
+                    });
+                    resolve(newResult);
+                })
+                .on("error", (err) => {
+                    reject(err); // Reject the Promise on error
+                });
+        }
+        catch(err){
+            console.log("Unable to import agent class data from CSV into the 'agentclass' table due to the following error: ",err)
+            reject(err);
+        }
+    });
+}
+
+(async () => {
+    try {
+      const confirmed = await askForConfirmation();
+      if (!confirmed) {
+        console.log("Operation canceled by the user.");
+        await prisma.$disconnect();
+        process.exit(0);
+      }
+  
+      const result = await main();
+      await saveAgentClass(result);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  })();
